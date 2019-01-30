017d7335279e0d7a5724dd36f52dc31380780673
SergioMejia2 <46963589+SergioMejia2@users.noreply.github.com>
2019-01-30 08:53:41 -0500

    Add files via upload

---
+++ bisecc.py
@@ -0,0 +1,32 @@
+import math
+
+def f(x):
+    return x*(math.e**x) - math.pi
+
+def biseccion(a=0, b=2, err=10e-8):
+    it = 0
+    a_i = a
+    b_i = b
+    x_i = f(a_i)
+    f_a = f(a_i)
+    f_b = f(b_i)
+    c_i = (a_i+b_i)/2
+    if f_a*f_b < 0:
+        while abs(x_i) > err:
+            f_a = f(a_i)
+            f_b = f(b_i)
+            c_i = (a_i+b_i)/2.0
+            x_c_i = f(c_i)
+            print(x_c_i)
+            if(abs(x_c_i) > err):
+                if x_c_i*f_a < 0 :
+                    b_i = c_i
+                elif x_c_i*f_b < 0 :
+                    a_i = c_i
+            else:
+                x_i = x_c_i
+            it = it + 1
+    return (c_i, it)
+
+(val,it) = biseccion()
+print("El valor es", val, "y las iteraciones son", it)
